833de0269b8b93cfd02ee2221d2d8804e9daf558
Muhsingenius <muhsingenius@gmail.com>
2024-11-23 23:20:51 +0000

    latest collection and best selling product add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     
     <div className='flex items-center justify-between py-5 font-small'>
         
-          <img src={assets.logo} className='w-36' alt="" />
+          <Link to={'/'}><img src={assets.logo} className='w-36' alt="" /></Link>
 
           <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
             <NavLink to={'/'} className='flex flex-col item-center gap-1'>
